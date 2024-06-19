4e6ced5b0e13e7ae215e76859dca8e533f92c9ba
FadiAntwan95 <antwan.fadi95@gmail.com>
2024-06-19 15:12:07 +0200

    wild-serirs-v4

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,22 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+
+CREATE TABLE category (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE program (
+    id INT PRIMARY KEY,
+    title VARCHAR(255),
+    synopsis text not null,
+    poster VARCHAR(255),
+    country VARCHAR(255),
+    year INT,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+
